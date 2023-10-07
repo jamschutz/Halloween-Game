4a5f2ae624ba2a89479725bed07bc86929f79de3
Hao <chliao1015@gmail.com>
2023-10-07 18:21:44 +0800

    auto generated level

--- Assets/Scripts/GenerateLevel.cs
+++ Assets/Scripts/GenerateLevel.cs
@@ -6,15 +6,27 @@ public class GenerateLevel : MonoBehaviour
 {
     public string[] words;
     public GameObject wordBrickPrefab;
+    public float levelRangeX, levelRangeZ;
+    public float groundScalingMax, groundScalingMin;
+    private GameObject player;
 
     private void Start()
     {
+        player = GameObject.Find("Player");
+
         foreach (string word in words)
         {
             GameObject wordBricks = Instantiate(wordBrickPrefab);
             wordBricks.GetComponent<CollideWithWords>().theWord = word;
+            wordBricks.transform.position = new Vector3(
+                Random.Range(player.transform.position.x+levelRangeX / 2, player.transform.position.x - levelRangeX / 2), 
+                0.5f, 
+                Random.Range(player.transform.position.z + levelRangeZ / 2, player.transform.position.z - levelRangeZ / 2));
+
+            GameObject cylinder = wordBricks.transform.Find("Cylinder").gameObject;
+            cylinder.transform.localScale = new Vector3(
+                Random.Range(groundScalingMax, groundScalingMin), 1, Random.Range(groundScalingMax, groundScalingMin));
+
         }
     }
-
-
 }
