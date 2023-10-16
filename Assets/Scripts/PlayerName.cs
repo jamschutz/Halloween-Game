4127a56197d30c52828a2b360f426967fee5e0e9
Hao <chliao1015@gmail.com>
2023-10-16 21:34:50 +0800

    timer

--- Assets/Scripts/PlayerName.cs
+++ Assets/Scripts/PlayerName.cs
@@ -9,17 +9,33 @@ public class PlayerName : MonoBehaviour
     public TextMeshProUGUI inputText;
     public TextMeshPro nameText;
     public Camera mainCamera;
+    public float secondsYouCanPlay;
+    public float timer;
 
     private GenerateLevel levelGenerator;
     private EpitaphManager epitaphManager;
     private TempPlayerController tempPlayerController;
     private Camera secondCamera;
+    private bool gameStart;
     private void Start()
     {
         levelGenerator = GetComponent<GenerateLevel>();
         epitaphManager = GameObject.Find("epitaphText").GetComponent<EpitaphManager>();
         tempPlayerController = GameObject.FindWithTag("Player").GetComponent<TempPlayerController>();
         secondCamera = GameObject.Find("secondCamera").GetComponentInParent<Camera>();
+        timer = 0;
+    }
+
+    private void Update()
+    {
+        if(gameStart)timer += Time.deltaTime;
+
+
+        if(timer > secondsYouCanPlay)
+        {
+            epitaphManager.EndGame();
+        }
+
     }
 
     public void ChangeTheNameOnStone()
@@ -39,6 +55,7 @@ public class PlayerName : MonoBehaviour
         tempPlayerController.enabled = true;
         mainCamera.gameObject.SetActive(true);
         secondCamera.gameObject.SetActive(false);
+        gameStart = true;
     }
 
 
