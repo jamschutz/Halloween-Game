fa5b713a6ffaf4febd65da6cd1dd44537ea7790c
Hao <chliao1015@gmail.com>
2023-10-16 19:17:35 +0800

    template progression

--- Assets/Scripts/EpitaphManager.cs
+++ Assets/Scripts/EpitaphManager.cs
@@ -16,13 +16,17 @@ public class EpitaphManager : MonoBehaviour
     public int howManyTemplate;
 
     private int[] epitaphTemplateIndex;
-    private int wordsForThisEpitaph;
-    private TextMeshProUGUI epitaphText;
+    private int howManywordsYouGot;
+    private TextMeshProUGUI epitaphText01;
+    private TextMeshProUGUI epitaphText02;
+    private TextMeshProUGUI epitaphText03;
     private Coroutine displayWordCorountine;
 
     private void Start()
     {
-        epitaphText = GetComponent<TextMeshProUGUI>();
+        epitaphText01 = GetComponent<TextMeshProUGUI>();
+        epitaphText02 = transform.Find("epitaphText02").GetComponent<TextMeshProUGUI>();
+        epitaphText03 = transform.Find("epitaphText03").GetComponent<TextMeshProUGUI>();
 
         //pick three
         epitaphTemplateIndex= new int[howManyTemplate];
@@ -35,10 +39,10 @@ public class EpitaphManager : MonoBehaviour
         for (int j = 0; j < howManyTemplate; j++)
         {
             epitaphTemplateIndex[j] = Random.Range(0, howManyTemplate);
-            while (epitaphTemplateIndex.Contains(epitaphTemplateIndex[j]))
+            /*while (epitaphTemplateIndex.Contains(epitaphTemplateIndex[j]))
             {
                 epitaphTemplateIndex[j] = Random.Range(0, howManyTemplate);
-            }
+            }*/
         }
 
         currentEpitaph01 = epitaphTemplate[epitaphTemplateIndex[0]];
@@ -46,27 +50,60 @@ public class EpitaphManager : MonoBehaviour
         currentEpitaph03 = epitaphTemplate[epitaphTemplateIndex[2]];
 
 
-        epitaphText.text = currentEpitaph01;
-        wordsForThisEpitaph = 0;
+        epitaphText01.text = currentEpitaph01;
+        howManywordsYouGot = 0;
     }
 
     public void AddNewWords(string newWords)
     {
-        wordsForThisEpitaph++;
-        if (wordsForThisEpitaph >= 3)
-        {
-            wordsForThisEpitaph = 0;
+        howManywordsYouGot++;
 
+        if (howManywordsYouGot == 4)
+        {
+            epitaphText02.text = currentEpitaph02;
+        }
+        if(howManywordsYouGot == 7)
+        {
+            epitaphText03.text = currentEpitaph03;
         }
 
-        if (displayWordCorountine != null)
+
+        if (howManywordsYouGot>=7)
         {
-            StopCoroutine(displayWordCorountine);
-            epitaphText.text = currentEpitaph01;
+            if (displayWordCorountine != null)
+            {
+                StopCoroutine(displayWordCorountine);
+                epitaphText03.text = currentEpitaph03;
+            }
+
+            currentEpitaph03 += newWords;
+            displayWordCorountine = StartCoroutine(TypeOutNewWords(newWords));
+        }
+        else if (howManywordsYouGot<7 && howManywordsYouGot>=4)
+        {
+            if (displayWordCorountine != null)
+            {
+                StopCoroutine(displayWordCorountine);
+                epitaphText02.text = currentEpitaph02;
+            }
+
+            currentEpitaph02 += newWords;
+            displayWordCorountine = StartCoroutine(TypeOutNewWords(newWords));
+        }
+        else
+        {
+            if (displayWordCorountine != null)
+            {
+                StopCoroutine(displayWordCorountine);
+                epitaphText01.text = currentEpitaph01;
+            }
+
+            currentEpitaph01 += newWords;
+            displayWordCorountine = StartCoroutine(TypeOutNewWords(newWords));
         }
 
-        currentEpitaph01 += newWords;
-        displayWordCorountine = StartCoroutine(TypeOutNewWords(newWords));
+
+            
         
     }
 
@@ -74,7 +111,21 @@ public class EpitaphManager : MonoBehaviour
     {
         foreach (char c in line.ToCharArray())
         {
-            epitaphText.text += c;
+            if (howManywordsYouGot >= 7)
+            {
+                epitaphText03.text += c;
+            }
+            else if(howManywordsYouGot<7 && howManywordsYouGot >= 4)
+            {
+                epitaphText02.text += c;
+            }
+            else
+            {
+                epitaphText01.text += c;
+            }
+
+
+            
             yield return new WaitForSeconds(typingSpeed);
         }
     }
