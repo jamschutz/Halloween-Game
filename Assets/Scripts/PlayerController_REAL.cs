cc0b11a6f76d50e04a7932159348ab1fc90b143c
Joey Schutz <schutzjoey@gmail.com>
2023-10-16 10:46:03 -0400

    new controller

--- Assets/Scripts/PlayerController_REAL.cs
+++ Assets/Scripts/PlayerController_REAL.cs
@@ -7,32 +7,37 @@ public class PlayerController_REAL : MonoBehaviour
     public float movingSpeed;
     public float jumpForce;
     public int numJumpsAllowed;
+    public float turnForce;
 
     private Rigidbody rb;
     private int jumpsUsed;
+    private float angleX;
+    private float angleY;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         jumpsUsed = 0;
+        angleX = transform.eulerAngles.x;
+        angleY = transform.eulerAngles.y;
     }
     void Update()
     {
         if (Input.GetKey(KeyCode.W))
         {
-            rb.AddForce(0, 0, movingSpeed);
+            rb.AddRelativeForce(0, 0, movingSpeed);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            rb.AddForce(0, 0, -movingSpeed);
+            rb.AddRelativeForce(0, 0, -movingSpeed);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            rb.AddForce(movingSpeed, 0, 0);
+            rb.AddRelativeForce(movingSpeed, 0, 0);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            rb.AddForce(-movingSpeed, 0, 0);
+            rb.AddRelativeForce(-movingSpeed, 0, 0);
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && jumpsUsed < numJumpsAllowed)
@@ -41,6 +46,24 @@ public class PlayerController_REAL : MonoBehaviour
             jumpsUsed++;
         }
 
+
+        // rotate left
+        if(Input.GetKey(KeyCode.LeftArrow)) {
+            angleY -= turnForce * Time.deltaTime;
+        }        
+        // rotate right
+        if(Input.GetKey(KeyCode.RightArrow)) {
+            angleY += turnForce * Time.deltaTime;
+        }
+        // rotate up
+        if(Input.GetKey(KeyCode.UpArrow)) {
+            angleX -= turnForce * Time.deltaTime;
+        }
+        // rotate down
+        if(Input.GetKey(KeyCode.DownArrow)) {
+            angleX += turnForce * Time.deltaTime;
+        }
+        transform.eulerAngles = new Vector3(angleX, angleY, transform.eulerAngles.z);
 
     }
 }
