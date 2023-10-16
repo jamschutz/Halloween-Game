5b5b9546e77138a608f3ee8ce27f1941196187e4
Joey Schutz <schutzjoey@gmail.com>
2023-10-16 10:07:56 -0400

    mouse input

--- Assets/Scripts/PlayerController_MOUSE.cs
+++ Assets/Scripts/PlayerController_MOUSE.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class PlayerController_MOUSE : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float turnForce;
+    public float hoverForce;
+
+    private Rigidbody rb;
+    private float angleX;
+    private float angleY;
+
+    private string allKeys = "`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./";
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        angleX = transform.eulerAngles.x;
+        angleY = transform.eulerAngles.y;
+
+        Cursor.visible = false;
+    }
+
+
+    private void Update()
+    {
+        Debug.Log($"got mouse x: {Input.GetAxis("Mouse X")}");
+        Debug.Log($"got mouse y: {Input.GetAxis("Mouse Y")}");
+        angleX += turnForce * Input.GetAxis("Mouse Y") * Time.deltaTime;
+        angleY += turnForce * Input.GetAxis("Mouse X") * Time.deltaTime;
+        transform.eulerAngles = new Vector3(angleX, angleY, transform.eulerAngles.z);
+
+        foreach(var key in allKeys) {
+            // Debug.Log("getting key: " + key);
+            if(Input.GetKeyDown(key.ToString())) {
+                rb.AddForce(transform.up * hoverForce, ForceMode.Impulse);
+            }
+        }
     }
 }
--- Assets/Scripts/PlayerName.cs
+++ Assets/Scripts/PlayerName.cs
@@ -14,14 +14,14 @@ public class PlayerName : MonoBehaviour
 
     private GenerateLevel levelGenerator;
     private EpitaphManager epitaphManager;
-    private PlayerController_NEW tempPlayerController;
+    private PlayerController_MOUSE tempPlayerController;
     private Camera secondCamera;
     private bool gameStart;
     private void Start()
     {
         levelGenerator = GetComponent<GenerateLevel>();
         epitaphManager = GameObject.Find("epitaphText").GetComponent<EpitaphManager>();
-        tempPlayerController = GameObject.FindWithTag("Player").GetComponent<PlayerController_NEW>();
+        tempPlayerController = GameObject.FindWithTag("Player").GetComponent<PlayerController_MOUSE>();
         secondCamera = GameObject.Find("secondCamera").GetComponentInParent<Camera>();
         
     }
